ce64eb31583c5966762989ede80bfa04279a67c5
Lzcom <Lizbeidyconeo@hotmail>
2024-01-21 09:14:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BlinkSeats',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
