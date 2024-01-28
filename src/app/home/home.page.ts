1dd23c4f0a4856eaef3ec88dec160785a93bebd8
Lzcom <lizbeidyconeo@hotmail.com>
2024-01-27 19:52:42 -0500

    Slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { IonButton } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,27 +10,36 @@ import { Router } from '@angular/router';
 export class HomePage {
   slides = [
     {
-      title: "Bienvenido a BlinkSeats",
-      description: "¡Donde podras apoyar a tu equipo en primera fila!",
-      image: "https://cdn-icons-png.flaticon.com/512/2817/2817809.png",
+      title: "BIENVENIDO A",
+      image: "../../assets/images/descargar.png",
+      description: "Boletos en segundos, diversión en minutos.",
       class: "slide-1",
-      color: '#2b2d42',
+      color: '#22004c',
       
     },
     {
-      title: "Tus eventos favoritos en un solo lugar",
+      title: "¡Apoyamos tu pasión!",
+      description: "¡Donde podras apoyar a tu equipo en primera fila!",
+      image: "https://cdn-icons-png.flaticon.com/512/2817/2817809.png",
+      class: "slide-2",
+      color: '#ffd30d',
+      
+    },
+    {
+      title: "¡Tus eventos favoritos en un solo lugar!",
       description: "Conciertos, teatro, deportes, festivales y mucho más",
       image: "https://cdn-icons-png.flaticon.com/512/195/195123.png",
-      class: "slide-2",
-      color: '#8d99ae',
+      class: "slide-3",
+      color: '#22004c',
       
     },
     {
       title: "¡Ingresa y cumple tus sueños!",
       description: "Entradas a eventos de forma facil y segura",
       image: "https://i.pinimg.com/originals/74/ed/af/74edaf05172513f7a3a09459847f3e57.png",
-      class: "slide-3",
-      color: '#ee4242', 
+      class: "slide-4",
+      color: '#ffd30d',
+       
     }
   ]
 
