271e932d8acd35cd4383c9377922e3765e50f40c
Lzcom <lizbeidyconeo@hotmail.com>
2024-01-29 23:10:42 -0500

    Codigo Nuevo

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,FormControl, Validators } from '@angular/forms';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -7,10 +8,31 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+loginForm: FormGroup;
+  constructor(private formBuilder:FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
+          )
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+        ])
+      )
+    })
+   }
 
   ngOnInit() {
   }
+login(login_data: any){
+  console.log(login_data);
+}
 
 }
